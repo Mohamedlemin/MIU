d15cc4771a10d4d111ff3a0e7bf752b0888b4d90
Mohamedlemin <med3744lemin@gmail.com>
2023-02-27 11:10:32 -0600

    First app

---
+++ src/FPP/Lab1/hello.java
@@ -0,0 +1,9 @@
+package FPP.Lab1;
+
+import javax.swing.*;
+
+public class hello {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello Mohamed lemin");
+    }
+}
